86d62335c91006044647ea601f615cbbb3e9921c
LightningYTlol <73615108+LightningYTlol@users.noreply.github.com>
2022-10-17 10:33:11 -0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Discord from "@/views/Discord.vue";
 
-const base = "/lsm";
+const base = "/";
 
 const routes = [
     {
-        path: base + "/home",
+        path: base,
         name: "Home",
         component: Home,
     },
-
-    { path: base, redirect: base + "/home" },
-    { path: "/", redirect: base + "/home" },
     { path: base + "discord", component: Discord }
 
 
