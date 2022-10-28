e0053d43312bb0745cae7233f459a041c15eb324
LightningYTlol <73615108+LightningYTlol@users.noreply.github.com>
2022-10-28 14:08:00 -0700

    Update state.js

--- src/components/sidebar/state.js
+++ src/components/sidebar/state.js
@@ -1,7 +1,7 @@
 import { ref, computed } from 'vue'
 
 export const collapsed = ref(true)
-export const toggleSidebar = () => (collapsed.value = !collapsed.value)
+export const toggleSidebar = () => (!collapsed.value = collapsed.value)
 
 export const SIDEBAR_WIDTH = 180
 export const SIDEBAR_WIDTH_COLLAPSED = 60
