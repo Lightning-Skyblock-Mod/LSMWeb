6542c3566c6c1d35edc6ceaba3dd4882808e7a49
LightningYTlol <unconfigured@null.spigotmc.org>
2022-10-28 23:00:16 -0700

    Fix gradient

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <div class="gradient__bg">
     <Loading v-if="isLoading" />
     <Sidebar />
     <div style="{'margin-left': sidebarWidth}">
@@ -9,7 +8,7 @@
 
 
 
-  </div>
+
 
 </template>
 
@@ -54,21 +53,6 @@ height: 100%;
 </style>
 
 <style scoped>
-.gradient__bg {
-  background:-moz-radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%);
-
-  /* safari 5.1+,chrome 10+ */
-  background:-webkit-radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%);
-
-  /* opera 11.10+ */
-  background:-o-radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%);
-
-  /* ie 10+ */
-  background:-ms-radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%);
-
-  /* global 92%+ browsers support */
-  background:radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%);
-}
 </style>
 
 
